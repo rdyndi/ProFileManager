cf74910e9e1c340cb17fe3074db81cfaf65136d9
rdyndi <rdyndi@gmail.com>
2025-11-24 22:58:18 +0700

    Update firebaseService.ts

--- services/firebaseService.ts
+++ services/firebaseService.ts
@@ -1,6 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// KONFIG DOMPETPINTAR PUNYAMU
 const firebaseConfig = {
   apiKey: "AIzaSyClChxcL4T1Rc3b8l6Zobbj6ynAQLWPGwY",
   authDomain: "dompetpintar-d50e0.firebaseapp.com",
@@ -8,9 +9,11 @@ const firebaseConfig = {
   storageBucket: "dompetpintar-d50e0.firebasestorage.app",
   messagingSenderId: "804793025772",
   appId: "1:804793025772:web:eb3ecc3f4edd030b86d1c9",
-  measurementId: "G-3BV80BW6ZN"
+  measurementId: "G-3BV80BW6ZN",
 };
 
-const app = initializeApp(firebaseConfig);
+// Biar nggak double init kalau dipanggil beberapa kali
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
+// Firestore instance yang dipakai storage.ts
 export const db = getFirestore(app);

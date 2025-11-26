aa210fcc95bfc3d380f917a5527ee46c17855648
rdyndi <rdyndi@gmail.com>
2025-11-26 23:31:03 +0700

    Update InvoiceGenerator.tsx

--- components/InvoiceGenerator.tsx
+++ components/InvoiceGenerator.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { Client, Invoice, InvoiceItem } from '../types';
 import { Printer, Search, Calendar, Plus, Trash2, Save, ArrowLeft, CreditCard, Clock } from 'lucide-react';
